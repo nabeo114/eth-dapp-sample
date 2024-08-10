23568d16bc370235cf3730205e6e21459f648ae6
nabeo114 <49563792+nabeo114@users.noreply.github.com>
2024-08-10 21:51:55 +0900

    amoyテストネットを追加

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -9,5 +9,9 @@ module.exports = {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [process.env.ACCOUNT_PRIVATE_KEY],
     },
+    amoy: {
+      url: `https://polygon-amoy.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    },
   },
 };
